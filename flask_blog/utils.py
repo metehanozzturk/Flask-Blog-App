963dfd67501d541f4294797d8a558438dfb5e7ae
Metehan Öztürk <metehanozzturk@gmail.com>
2024-10-06 22:01:29 +0300

    Update utils.py

--- flask_blog/utils.py
+++ flask_blog/utils.py
@@ -1,9 +1,9 @@
 from itsdangerous import URLSafeTimedSerializer as Serializer
 
-# Use a string for the secret key
+
 s = Serializer(b'secret', 30)
 
-# Generate the token, and remove the decode() call, as dumps() now returns a string in recent versions
+
 token = s.dumps({'user_id': 1})
 
 print(token)
